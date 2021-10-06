d06563e880fa369038d0c7f022b40e4e60f7c48f
Hirochika Matsumoto <git@hkmatsumoto.com>
2021-10-06 16:19:30 +0900

    Don't emit warning if the word `illegal` appeared inside placeholders

--- src/rules/illegals.rs
+++ src/rules/illegals.rs
@@ -1,4 +1,7 @@
 use annotate_snippets::snippet::{Annotation, AnnotationType, Slice, Snippet, SourceAnnotation};
+use nom::Finish;
+
+use crate::parse::{parse_message, Element};
 
 pub(crate) struct Illegals {}
 
@@ -8,30 +11,38 @@ impl super::Rule for Illegals {
     }
 
     fn check(&self, diag: &crate::json::Diagnostic, ctx: &mut crate::lint::LintCtx) {
-        if let Some(idx) = diag.message.find("illegal") {
-            let (lo, hi) = (idx, idx + "illegal".len());
-            let snip = Snippet {
-                title: Some(Annotation {
-                    label: Some("the word `illegal` is illegal"),
-                    id: None,
-                    annotation_type: AnnotationType::Warning,
-                }),
-                footer: vec![],
-                slices: vec![Slice {
-                    source: diag.message.as_str(),
-                    line_start: 1,
-                    fold: false,
-                    origin: None,
-                    annotations: vec![SourceAnnotation {
-                        label: "consider using more specific word, like `invalid`",
-                        range: (lo, hi),
-                        annotation_type: AnnotationType::Warning,
-                    }],
-                }],
-                ..Default::default()
-            };
+        let sentences = parse_message(&diag.message).finish().unwrap().1;
 
-            ctx.emit(snip);
+        for elem in sentences.into_iter().flatten() {
+            if let Element::Normal(n) = elem.inner() {
+                if let Some(idx) = n.find("illegal") {
+                    let offset = elem.span().location_offset() + idx;
+                    let (lo, hi) = (offset, offset + "illegal".len());
+
+                    let snip = Snippet {
+                        title: Some(Annotation {
+                            label: Some("the word `illegal` is illegal"),
+                            id: None,
+                            annotation_type: AnnotationType::Warning,
+                        }),
+                        footer: vec![],
+                        slices: vec![Slice {
+                            source: diag.message.as_str(),
+                            line_start: 1,
+                            fold: false,
+                            origin: None,
+                            annotations: vec![SourceAnnotation {
+                                label: "consider using more specific word, like `invalid`",
+                                range: (lo, hi),
+                                annotation_type: AnnotationType::Warning,
+                            }],
+                        }],
+                        ..Default::default()
+                    };
+
+                    ctx.emit(snip);
+                }
+            }
         }
     }
 }
